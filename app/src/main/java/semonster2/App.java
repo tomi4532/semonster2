f9ad39b3ee4768ef10928f331da4a8b7d7fb299e
Tsubasa Tomitaka <tomituba098@outlook.com>
2023-07-03 10:04:01 +0900

    Helloworldを変更

--- app/src/main/java/semonster2/App.java
+++ app/src/main/java/semonster2/App.java
@@ -4,11 +4,11 @@
 package semonster2;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+  public String getGreeting() {
+    return "こんにちは SEMonster";
+  }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-    }
+  public static void main(String[] args) {
+    System.out.println(new App().getGreeting());
+  }
 }
